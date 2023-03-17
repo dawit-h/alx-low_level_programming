7f949c342d8a89155ac404828ae1bac0f3a0004f
Dawberry22 <dhailu125@gmail.com>
2023-03-17 12:13:21 +0300

    done 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
+	printf("Size of float: %f byte(s)", sizeof(float))
+	
+}
