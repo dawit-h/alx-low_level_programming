5a584a77807c79f63a77128e08005eb82d4bada2
Dawit Hailu <125505424+dawit-h@users.noreply.github.com>
2023-03-21 11:50:31 +0000

    done task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
