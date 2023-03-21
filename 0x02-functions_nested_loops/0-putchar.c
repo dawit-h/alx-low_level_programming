c1cc582dc168de2ab70a73a0388a0e08e0cf6b89
Dawit Hailu <125505424+dawit-h@users.noreply.github.com>
2023-03-21 11:40:14 +0000

    add _putchar and task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *c = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
